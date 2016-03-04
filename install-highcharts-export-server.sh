34548cf5cdada88f83bb194037515513bd5ec28b
indra sadik <g3n1k@yahoo.com>
2016-03-04 20:24:15 +0700

    fix some path

--- install-highcharts-export-server.sh
+++ install-highcharts-export-server.sh
@@ -31,8 +31,8 @@ cd /usr/local/share
 sudo wget https://phantomjs.googlecode.com/files/phantomjs-1.9$
 tar xjf phantomjs-1.9.0-linux-x86_64.tar.bz2
 sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
-sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
-sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x86_64/bin/phantomjs /usr/bin/share/phantomjs
 
 #check if phantomjs was successfully installed
 phantomjs -v
